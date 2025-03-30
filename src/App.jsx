d41eebf01e2c640cad361ba9db39f1f4cc5a0a2f
Pawan Rathee <ratheepawan666@gmail.com>
2025-03-30 11:19:42 +0530

    Start making  new Todo List Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="bg-red-600">
+        <h1 className="text-white text-center text-4xl p-4">
+          Hello, Tailwind CSS
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
